45d8be3c5f31a33bf22c9c5ddf642f7a6b9a1e33
Piti.C <piti_fee@hotmail.com>
2022-03-17 22:22:39 +0700

    update azure func

--- api/classify/__init__.py
+++ api/classify/__init__.py
@@ -1,29 +1,35 @@
 import logging
 import os
+
+from numpy import outer
 import azure.functions as func
 import pickle
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        pass
+    else:
+        temp = req_body.get('temp')
+        outlook = req_body.get('outlook')
+        humidity = req_body.get('humidity')
+        windy = req_body.get('windy')
 
     filename = 'play_tennis.pkl'
     script_dir = os.path.dirname(__file__)
     filepath = os.path.join(script_dir, filename)
     model = pickle.load(open(filepath, 'rb'))
-    answer = model.predict([[2,1,0,0]])
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    answer = model.predict([[outlook,temp,humidity,windy]])
+
+    if answer == 0:
+        answer = "Yes"
     else:
-        return func.HttpResponse(
-             f"Answer: {answer}.",
-             status_code=200
-        )
+        answer = "No"
+
+    return func.HttpResponse(
+            f"Answer: {answer}.",
+            status_code=200
+    )
